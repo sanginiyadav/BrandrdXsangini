6d792a0f2954638e28efbd95e7c3cc95adcd5405
sanginiyadav <sanginiyadav98@gmail.com>
2025-10-10 20:49:57 +0530

    Update zowner.py

--- BrandrdXMusic/plugins/tools/zowner.py
+++ BrandrdXMusic/plugins/tools/zowner.py
@@ -17,7 +17,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://t.me/NOBITA_BOTS"
+                        "ƨσʋяcɛ", url=f"https://t.me/NOBITAXD7"
                     )
                 ]
             ]
@@ -34,7 +34,7 @@ async def clones(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "ƨσʋяcɛ", url=f"https://t.me/NOBITA_BOTS"
+                        "ƨσʋяcɛ", url=f"https://t.me/NOBITAXD7"
                     )
                 ]
             ]
